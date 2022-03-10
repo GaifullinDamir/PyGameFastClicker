90869297cf68147d0e1469d23f4d2fc41dbadc55
Damir <damiros.20002@yandex.ru>
2022-03-10 13:48:24 +0300

    Work in progress

--- PyGameFastClicker/PyGameFastClicker.py
+++ PyGameFastClicker/PyGameFastClicker.py
@@ -2,32 +2,31 @@ import pygame
 import time
 
 pygame.init()
-'''создаём окно программы'''
-back = (255, 255, 255) #цвет фона (background)
-mw = pygame.display.set_mode((500, 500)) #окно программы (main window)
-mw.fill(back)
-clock = pygame.time.Clock()
-
+backColor = (255, 255, 255) 
+mainWindow = pygame.display.set_mode((500, 500)) 
+mainWindow.fill(backColor)
+clock = pygame.time.Clock()  
 
 class Area():
-  def __init__(self, x=0, y=0, width=10, height=10, color=None):
-      self.rect = pygame.Rect(x, y, width, height) #прямоугольник
-      self.fill_color = color
-  def color(self, new_color):
-      self.fill_color = new_color
-  def fill(self):
-      pygame.draw.rect(mw, self.fill_color, self.rect)
-  def outline(self, frame_color, thickness): #обводка существующего прямоугольника
-      pygame.draw.rect(mw, frame_color, self.rect, thickness)   
-  def collidepoint(self, x, y):
-      return self.rect.collidepoint(x, y)      
+    def __init__(self, x=0, y=0, width=10, height=10, color = None):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.fillColor = color
+    def color(self,newColor):
+        self.fillColor = newColor
+    def fill(self):
+        pygame.draw.rect(mainWindow, self.fillColor, self.rect)
+    def outline(self, frameColor, thickness):
+        pygame.draw.rect(mainWindow, frameColor, self.rect, thickness)
+    def collidepoint(self, x, y):
+        return self.rect.collidepoint(x, y)
 
 class Label(Area):
-  def set_text(self, text, fsize=12, text_color=(0, 0, 0)):
-      self.image = pygame.font.SysFont('verdana', fsize).render(text, True, text_color)
-  def draw(self, shift_x=0, shift_y=0):
-      self.fill()
-      mw.blit(self.image, (self.rect.x + shift_x, self.rect.y + shift_y))
+    def setText(self, text, fsize = 12, textColor = (0, 0, 0)):
+        self.image = pygame.font.SysFont('verdana', fsize).render(text, True, textColor)
+    def draw(self, shiftX = 0, shiftY = 0):
+        self.fill()
+        mainWindow.blit(self.image, (self.rect.x + shiftX, self.rect.y +shiftY))
+
 RED = (255, 0, 0)
 GREEN = (0, 255, 51)
 YELLOW = (255, 255, 0)
@@ -38,10 +37,10 @@ num_cards = 4
 x = 70
 
 for i in range(num_cards):
-  new_card = Label(x, 170, 70, 100, YELLOW)
-  new_card.outline(BLUE, 10)
-  new_card.set_text('CLICK', 14)
-  cards.append(new_card)
+  newCard = Label(x, 170, 70, 100, YELLOW)
+  newCard.outline(BLUE, 10)
+  newCard.setText('CLICK', 14)
+  cards.append(newCard)
   x = x + 100
  
 wait = 0
